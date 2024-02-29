912a28d43a2a69b33f95dbd8a4ca727b457652c9
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-02-29 18:26:28 +0530

    added application struct for passing http.Handle interface

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,15 +7,24 @@ import (
 	"os"
 )
 
+type application struct{
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
+
 func main(){
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
 
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
 
 	mux := http.NewServeMux()
 
@@ -26,7 +35,13 @@ func main(){
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
