1d877f4e6c5d7aeb3fc5b3cf4f2ea399af4c9156
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2024-03-03 00:22:33 +0530

    added sql support some function related to its query

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -1,10 +1,13 @@
 package main
- 
-import(
+
+import (
+	"errors"
 	"fmt"
-	"html/template"
+	//"html/template"
 	"net/http"
 	"strconv"
+
+	"snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request){
@@ -13,25 +16,35 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.html",
-		"./ui/html/pages/home.html",
-		"./ui/html/partials/nav.html",
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
 	}
 
-	ts, err := template.ParseFiles(files...)
-	if err != nil{
-		app.serverError(w, err)
-	  http.Error(w, "Internal Server Error", 500)
-	  return 
+	for _, snippet := range snippets{
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
 
-	
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil{
-		app.serverError(w, err)
-	  http.Error(w, "Internal server error", 500)
-	}
+//	files := []string{
+//		"./ui/html/base.html",
+//		"./ui/html/pages/home.html",
+//		"./ui/html/partials/nav.html",
+//	}
+//
+//	ts, err := template.ParseFiles(files...)
+//	if err != nil{
+//		app.serverError(w, err)
+//	  http.Error(w, "Internal Server Error", 500)
+//	  return 
+//	}
+//
+//	
+//	err = ts.ExecuteTemplate(w, "base", nil)
+//	if err != nil{
+//		app.serverError(w, err)
+//	  http.Error(w, "Internal server error", 500)
+//	}
 
 }
 
@@ -39,9 +52,17 @@ func (app *application)snippetView(w http.ResponseWriter, r *http.Request){
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 0{
 	  app.notFound(w)
-	  return 
+	  return   
 	}
-	fmt.Fprintf(w, "The id is %d..", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil{
+		if errors.Is(err, models.ErrNoRecord){
+			app.notFound(w)
+		}else{
+			app.serverError(w, err)
+		}
+	}
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
@@ -50,6 +71,16 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
 		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+	
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil{
+		app.serverError(w, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,25 +5,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"snippetbox/internal/models"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct{
 	errorLog *log.Logger
 	infoLog *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main(){
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+	dsn := flag.String("dsn", "web:@/snippetbox?parseTime=true", "MySQL data source name")
 
+	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, err := openDB(*dsn)
+	if err != nil{
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -33,6 +48,17 @@ func main(){
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error){
+	db, err := sql.Open("mysql", dsn)
+	if err != nil{
+	  return nil, err
+	}
+	if err = db.Ping(); err != nil{
+		return nil, err
+	}
+	return db, nil
 }
